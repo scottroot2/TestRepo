83b1f4985e64c6bdf2429d342e2712420bcdb6ba
scottroot2 <scottroot2@gmail.com>
2016-01-15 15:19:03 -0500

    This is a test

---
+++ Hello World/Hello World/WorldView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class WorldView
+    {
+
+        public string Message { get; set; }
+
+        public WorldView()
+        {
+            Message = "Hello World";
+        }
+            
+    }
+}
